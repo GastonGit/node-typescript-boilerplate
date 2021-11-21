d77ebc5d210254273d4b385f0fc8b8be1a5f381b
GastonGit <73943682+GastonGit@users.noreply.github.com>
2021-11-21 16:05:34 +0100

    setup cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,17 @@
 import express, { Application } from 'express';
 import routes from './routes';
 import helmet from 'helmet';
+import cors from 'cors';
 
 const app: Application = express();
 const PORT = process.env.PORT || 8000;
 
+const corsOptions = {
+    origin: '*',
+};
+
 app.use(helmet());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(routes);
 
